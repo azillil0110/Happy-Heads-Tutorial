46bb28b7a461bd928f0750672e481ece385534b2
happydankengine <164768490+happydankengine@users.noreply.github.com>
2024-11-25 14:49:51 +0800

    yung student info

---
+++ studentinfo.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Student Information</title>
+    <link rel="stylesheet" href="studentinfo.css">
+</head>
+<body>
+    <header>
+        <div class="center-alignment">
+            <img id="logo" src="image/logo.jpg" alt="HPT Logo">
+            <h2>Happy Head Tutorials</h2>
+        </div>
+        
+        <nav>
+            <a class="a-nav" href="">Home</a>
+            <a class="a-nav" href="">Team</a>
+            <a class="a-nav" href="">Program</a>
+            <a class="a-nav" href="">About</a>
+            <a class="a-nav" href="">Contact</a>
+            <button class="btn-nav">APPLY</button>
+        </nav>
+    </header>
+
+    <div id="profile-picture-section">
+        <img id="profile-picture" src="https://via.placeholder.com/150" alt="Profile Picture">
+    </div>
+
+    <div id="body">
+        <div id="form-section">
+            <h2 class="subtitle">Personal Information</h2>
+            <hr>
+            <div class="row">
+                <div class="col-25">
+                    <label>First Name:</label>
+                    <div class="read-only">Jhon Ron</div>
+                </div>
+                <div class="col-25">
+                    <label>Last Name:</label>
+                    <div class="read-only">Eleazar</div>
+                </div>
+                <div class="col-25">
+                    <label>Nickname:</label>
+                    <div class="read-only">Jhonron</div>
+                </div>
+                <div class="col-25">
+                    <label>Gender:</label>
+                    <div class="read-only">Male</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label>Date of Birth:</label>
+                    <div class="read-only">2004-07-17</div>
+                </div>
+                <div class="col-25">
+                    <label>Age:</label>
+                    <div class="read-only">20</div>
+                </div>
+                <div class="col-25">
+                    <label>School:</label>
+                    <div class="read-only">Bulacan State University</div>
+                </div>
+                <div class="col-25">
+                    <label>Grade Level:</label>
+                    <div class="read-only">3rd Year College</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-50">
+                    <label>Home Address:</label>
+                    <div class="read-only">North Grove, San Simon, Pampanga</div>
+                </div>
+            </div>
+        </div>
+
+        <div id="form-section">
+            <h2 class="subtitle">Parent/Guardian Information</h2>
+            <hr>
+            <div class="row">
+                <div class="col-25">
+                    <label>Last Name:</label>
+                    <div class="read-only">Eleazar</div>
+                </div>
+                <div class="col-25">
+                    <label>First Name:</label>
+                    <div class="read-only">Jhonron</div>
+                </div>
+                <div class="col-25">
+                    <label>Relationship:</label>
+                    <div class="read-only">Twin</div>
+                </div>
+                <div class="col-25">
+                    <label>Contact Number:</label>
+                    <div class="read-only">+63 969 459 7654</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-50">
+                    <label>E-Mail:</label>
+                    <div class="read-only">jhonroneleazar17@gmail.com</div>
+                </div>
+            </div>
+        </div>
+
+        <div id="form-section">
+            <h2 class="subtitle">Additional Information</h2>
+            <hr>
+            <div class="row">
+                <div class="col-50">
+                    <label>Allergies or Medical Conditions:</label>
+                    <div class="read-only">Chocolates</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label>Allow Photos:</label>
+                    <div class="read-only">Yes</div>
+                </div>
+                <div class="col-25">
+                    <label>Comments:</label>
+                    <div class="read-only">hello po</div>
+                </div>
+            </div>
+        </div>
+
+        <div id="form-section">
+            <h2 class="subtitle">Pick-Up/Drop-Off Schedule</h2>
+            <hr>
+            <div class="row">
+                <div class="col-25">
+                    <label>Monday:</label>
+                    <div class="read-only">8:00 AM - 4:00 PM</div>
+                </div>
+                <div class="col-25">
+                    <label>Tuesday:</label>
+                    <div class="read-only">8:00 AM - 4:00 PM</div>
+                </div>
+                <div class="col-25">
+                    <label>Wednesday:</label>
+                    <div class="read-only">8:00 AM - 4:00 PM</div>
+                </div>
+                <div class="col-25">
+                    <label>Thursday:</label>
+                    <div class="read-only">8:00 AM - 4:00 PM</div>
+                </div>
+                <div class="col-25">
+                    <label>Friday:</label>
+                    <div class="read-only">8:00 AM - 4:00 PM</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
