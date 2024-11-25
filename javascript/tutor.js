3cfd92daf60b7f120a21f98c305b23d0757fa793
gumi-dm <miyukiando.0119@gmail.com>
2024-11-25 14:52:53 +0800

    1

--- javascript/tutor.js
+++ javascript/tutor.js
@@ -141,7 +141,7 @@ function getContent(section) {
                     </div>
                 </div>
                 <div class="righttop">
-                    <p class="righttoptext">Your Schedule</p>
+                    <p class="righttoptext">Your Schedulee</p>
                 </div>
                 <div class="rightbot">
                     <div class="row">
