1b0aa707725296b2973ed5ca2827e8073047f661
gumi-dm <miyukiando.0119@gmail.com>
2024-11-27 18:24:37 +0800

    edit size

--- admin-add-tutor.php
+++ admin-add-tutor.php
@@ -182,7 +182,7 @@
                    
                 </div>
                 <div class="col-20">
-                    <button class="btn" id="btn-save">Add More Schedule</button>
+                    <button class="btn" id="btn-save">Add Schedule</button>
                 </div>
                 <div class="col-30">
                     <button class="btn" id="btn-save">Add Tutor</button>
