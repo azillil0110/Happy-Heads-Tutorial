9f15f3c39d62db2ec309a074d6590de8a9e35dd7
johnl <johnlutherdelacruz@gmail.com>
2024-11-28 14:36:42 +0800

    fixed tutor

--- admin-tutor.php
+++ admin-tutor.php
@@ -6,37 +6,21 @@
 </div>
 <div class="rightbot">
     <?php 
-            $sql = "SELECT * FROM `moderator` WHERE acc_type != 'admin'";
-            $result = mysqli_query($conn, $sql);
-            $resultcheck = mysqli_num_rows($result);
+        $sql = "SELECT * FROM `moderator` WHERE acc_type != 'admin'";
+        $result = mysqli_query($conn, $sql);
+        $resultcheck = mysqli_num_rows($result);
 
-            if($resultcheck > 0){
-                $i = 1;
-                while($row = mysqli_fetch_assoc($result)){ 
+        if($resultcheck > 0){
+            $i = 1;
+            while($row = mysqli_fetch_assoc($result)){ 
 
-                    ?>
-                    <div class="box " onclick="toggleOverlay()">
-                        <div id="tutor-pic<?php echo $i; ?>" class="stpfp" 
-                             style="background-image: url('./images/team/<?php echo $row['pfp_url']; ?>');">
-                        </div>
-                        <p class="righttitle"><?php echo $row['mod_fname']; echo" "; echo $row['mod_lname'];?></p>
-                        <div class="hover-text">View Schedule</div>
+                ?>
+                <div class="box " onclick="toggleOverlay()">
+                    <div id="tutor-pic<?php echo $i; ?>" class="stpfp" 
+                            style="background-image: url('./images/team/<?php echo $row['pfp_url']; ?>');">
                     </div>
-                    <script>
-                        document.addEventListener('DOMContentLoaded', function () {
-                            console.log('DOM fully loaded');
-                            const imageContainer = document.getElementById('stud-pic<?php echo $i; ?>');
-                            if (imageContainer) {
-                                imageContainer.style.backgroundImage = "url('./images/students/<?php echo $row['pfp_url']; ?>')";
-                                imageContainer.style.backgroundSize = 'cover';
-                                imageContainer.style.backgroundPosition = 'center';
-                                imageContainer.style.backgroundRepeat = 'no-repeat';
-                            }
-                        });
-                    </script>
-                    <a href="admin-tutorinfo.php?id=<?php echo $tutorID; ?>" class="righttitle"> 
-                        <?php echo $row['mod_fname'] . " " . $row['mod_lname']; ?>
-                    </a>
+                    <p class="righttitle"><?php echo $row['mod_fname']; echo" "; echo $row['mod_lname'];?></p>
+                    <div class="hover-text">View Schedule</div>
                 </div>
                 <?php
                 $i++;
