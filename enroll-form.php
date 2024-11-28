6997c6bd613bac4bae983617e09a33d0dc4a8c4d
johnl <johnlutherdelacruz@gmail.com>
2024-11-28 17:32:45 +0800

    fixed the enrollment form

--- enroll-form.php
+++ enroll-form.php
@@ -315,42 +315,6 @@
         </div>
     </main>
 
-    <?php include('includes/footer.php') ?>
-    <?php 
-                $sql = "SELECT * FROM `events_`";
-                $result = mysqli_query($conn, $sql);
-                $resultcheck = mysqli_num_rows($result);
-
-                if($resultcheck > 0){
-                    $i =0;
-                    while($row = mysqli_fetch_assoc($result)){ 
-                        ?>
-                        <div>
-                            <div id="eventpic<?php echo $i; ?>" class="event-pic"></div>
-                            <script>
-                                    document.addEventListener('DOMContentLoaded', function () {
-                                    const imageContainer = document.getElementById('eventpic<?php echo $i; ?>');
-                                    if (imageContainer) {
-                                        imageContainer.style.backgroundImage = "url('./images/events/<?php echo $row['image_url']; ?>')";
-                                        imageContainer.style.backgroundSize = 'cover';
-                                        imageContainer.style.backgroundPosition = 'center';
-                                        imageContainer.style.backgroundRepeat = 'no-repeat';
-                                    }
-                                });
-                            </script>
-                            <h1><?php echo $row['event_name']?></h1>
-                            <p id="date"><?php 
-                                $date = $row['event_date'];
-                                echo substr($date,0,10);
-                            ?></p>
-                            <p id="description"><?php echo $row['event_description']?></p>
-                            <hr>
-                        </div>
-                        <?php
-                        $i++;
-                    }
-                }
-            ?>
         </div>
     </section>
 
