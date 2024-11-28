7ce13934d8bf4aa689b02b4e9505825b1823c523
johnl <johnlutherdelacruz@gmail.com>
2024-11-28 17:17:19 +0800

    add user done

--- includes/insert-user.php
+++ includes/insert-user.php
@@ -12,6 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
     $userdesc = $_POST['message'];
     $useremail = $_POST['email'];
     $userphone = $_POST['phone'];
+    $userdefaultpfp = 'default.jpg';
 
     echo "$userfname<br>";
     echo "$userlname<br>";
@@ -21,5 +22,29 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
     echo "$userdesc<br>";
     echo "$useremail<br>";
     echo "$userphone<br>";
+
+    try {
+        require_once 'inputdb-dbh.inc.php';
+
+        $query = "INSERT INTO moderator (mod_fname, mod_lname, mod_email, mod_phone, mod_bdate, mod_gender, mod_usern, mod_pass, acc_type, acc_status, mod_description, pfp_url) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
+        $modID = 1;
+
+        $stmt = $pdo->prepare($query);
+
+        $stmt->execute([$userfname, $userlname, $useremail, $userphone, $userbdate, $usergender, $userfname, $userlname,$useracctype,'active',$userdesc, $userdefaultpfp]);
+
+        $pdo = null;
+        $stmt = null;
+
+        // Ensure no output before this
+        header('Location: ../admin-dashboard.php?username=' . urlencode($username));
+        die();
+    } catch (PDOException $e) {
+        die("Query Failed:" . $e->getMessage());
+    }
+
+    
 
 }
