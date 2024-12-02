0eacc917595b7abcbea4faa151d5cf4ea90f1c59
gumi-dm <miyukiando.0119@gmail.com>
2024-12-03 00:46:08 +0800

    sched

--- tutor/schedule.php
+++ tutor/schedule.php
@@ -1,61 +1,114 @@
 <link rel="stylesheet" href="css/schedule.css">
-<div class="righttop">
-    <p class="righttoptext">My Schedule</p>
-</div>
-<div class="rightbot">
+
 <?php
+    include_once '../includes/dbh.inc.php';
+    session_start();
+
+    // Set the current user mod_id to 8
+    $currentUser =  $_SESSION['username'];
+
+    // Fetch schedules for the current tutor (mod_ID = 8)
     $sql = "SELECT 
-            sc.sched_id,
-            sc.stud_id,
-            sc.mod_id,
-            sc.sched_day,
-            sc.sched_starttime,
-            sc.sched_endtime
-            FROM
-                schedule sc
-                ";
+                sched_id, 
+                sched_day, 
+                sched_starttime, 
+                sched_endtime, 
+                stud_ID, 
+                mod_ID 
+            FROM schedule 
+            WHERE mod_ID = '$currentUser'
+            ORDER BY sched_day, sched_starttime";
+    
     $result = mysqli_query($conn, $sql);
-    $resultcheck = mysqli_num_rows($result);
-    if ($resultcheck > 0) {
-        $sc = 1;
-        while ($row = mysqli_fetch_assoc($result)) {
-            $schedID = $row["sched_id"];
-            ?>
-            <div class="box"
-                sched-id =  "<?php echo $row['sched_id']; ?>"
-                stud-id = "<?php echo $row['stud_id']; ?>"
-                mod-id = "<?php echo $row['mod_id']; ?>"
-                sched-day = "<?php echo $row['sched_day']; ?>"
-                starttime = "<?php echo $row['sched_starttime']; ?>"
-                endtime = "<?php echo $row['sched_endtime']; ?>">
-                <a href="admin-studentinfo.php?id=<?php echo $studentID; ?>" class="righttitle">
-                    <?php echo $row['stud_fname'] . " " . $row['stud_lname']; ?>
-                </a>
-                <div class="hover-text">More Info</div>
-            </div>
-            </div>
+    $schedules = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    // Function to merge overlapping schedules by day
+    function mergeSchedulesForDay($schedules) {
+        // Sort schedules by start time
+        usort($schedules, function($a, $b) {
+            return strtotime($a['sched_starttime']) - strtotime($b['sched_starttime']);
+        });
+
+
+        $mergedSchedules = [];
+        $currentSchedule = null;
+
+        foreach ($schedules as $schedule) {
+            $startTime = strtotime($schedule['sched_starttime']);
+            $endTime = strtotime($schedule['sched_endtime']);
+            echo $startTime;
+
+            // If no current schedule or the current schedule ends before the new one starts, add current schedule to the result
+            if (!$currentSchedule || $currentSchedule['sched_endtime'] <= $schedule['sched_starttime']) {
+                if ($currentSchedule) {
+                    $mergedSchedules[] = $currentSchedule;
+                }
+                $currentSchedule = $schedule;
+            } else {
+                // Merge if overlapping
+                if ($endTime > strtotime($currentSchedule['sched_endtime'])) {
+                    $currentSchedule['sched_endtime'] = date('H:i', $endTime);
+                }
             }
         }
-?>
-        <div class="rowcenter">    
-        <div class="col-16">
-            <p class="textlabel">MONDAY</p>
-        </div>
-        <div class="col-16">
-            <p class="textlabel">TUESDAY</p>
-        </div>
-        <div class="col-16">
-            <p class="textlabel">WEDNESDAY</p>
-        </div>
-        <div class="col-16">
-            <p class="textlabel">THURSDAY</p>
-        </div>
-        <div class="col-16">
-            <p class="textlabel">FRIDAY</p>
-        </div>
-        <div class="col-16">
-            <p class="textlabel">SATURDAY</p>
-        </div>
-    </div>
-</div>
 
+        if ($currentSchedule) {
+            $mergedSchedules[] = $currentSchedule;
+        }
+
+        return $mergedSchedules;
+    }
+
+    // Group schedules by day of the week
+    $daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    $groupedSchedules = array_fill_keys($daysOfWeek, []);
+
+    foreach ($schedules as $schedule) {
+        $groupedSchedules[$schedule['sched_day']][] = $schedule;
+    }
+
+    // Display the schedule table
+    echo '<div class="schedule-table">';
+
+    // Table Header for Days of the Week
+    echo '<div class="schedule-row">';
+    foreach ($daysOfWeek as $day) {
+        echo "<div class='schedule-cell'>$day</div>";
+    }
+    echo '</div>';
+
+    // Define possible time slots (modify as needed)
+    $timeSlots = ['09:00 - 11:00', '11:00 - 13:00', '13:00 - 15:00', '15:00 - 17:00'];
+
+    // Loop through the time slots to create rows
+    foreach ($timeSlots as $timeSlot) {
+        echo '<div class="schedule-row">';
+        foreach ($groupedSchedules as $day => $schedules) {
+            $mergedSchedules = mergeSchedulesForDay($schedules);
+
+            $found = false;
+            foreach ($mergedSchedules as $schedule) {
+                // Check if this schedule fits within the current time slot
+                $scheduleStart = strtotime($schedule['sched_starttime']);
+                $scheduleEnd = strtotime($schedule['sched_endtime']);
+                $slotStart = strtotime(explode(' - ', $timeSlot)[0]);
+                $slotEnd = strtotime(explode(' - ', $timeSlot)[1]);
+
+                // If the schedule's time overlaps with the current time slot, display it
+                if (($scheduleStart >= $slotStart && $scheduleStart < $slotEnd) || ($scheduleEnd > $slotStart && $scheduleEnd <= $slotEnd)) {
+                    // Display the merged schedule without the student ID
+                    echo "<div class='schedule-cell'>Start: {$schedule['sched_starttime']}<br>End: {$schedule['sched_endtime']}</div>";
+                    $found = true;
+                    break;
+                }
+            }
+
+            if (!$found) {
+                echo "<div class='schedule-cell'></div>"; // Empty cell if no schedule matches the time slot
+            }
+        }
+        echo '</div>';
+    }
+
+    echo '</div>';
+?>
