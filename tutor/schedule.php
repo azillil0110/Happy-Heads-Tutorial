a4a516ffde07ad3e55aa0c4c13df477aea89341e
gumi-dm <miyukiando.0119@gmail.com>
2024-12-02 12:42:24 +0800

    SCHED

--- tutor/schedule.php
+++ tutor/schedule.php
@@ -1,3 +1,10 @@
-<?php
+<link rel="stylesheet" href="css/schedule.css">
+<div class="righttop">
+    <p class="righttoptext">My Schedule</p>
+</div>
+<div class="rightbot">
+    <div class="row">
+        <h1 id="texttile">Schedule</h1>
+    </div>
+</div>
 
-echo "this is the schedule php";
