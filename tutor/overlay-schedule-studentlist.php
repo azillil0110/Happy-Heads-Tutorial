0118a8a1a7878c1b678de93447fc5a58c00bb052
gumi-dm <miyukiando.0119@gmail.com>
2024-12-04 01:12:40 +0800

    student list schedule

--- tutor/overlay-schedule-studentlist.php
+++ tutor/overlay-schedule-studentlist.php
@@ -3,18 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/schedule.css">
+    <link rel="stylesheet" href="css/-schedule.css">
     <title>Student List</title>
 </head>
 <body>
 <div id="student-list-overlay" class="overlay">
     <div class="overlay-content">
+        
+        <div class="listcontent">
+            
+            <div class="row">
+            <div class="col-10"></div>
+            <div class="col-80">
+                <h2>List of Students</h2>
+            </div>
+            <div class="col-10"></div>
+            </div>
+            <div class="row" id="list">
+            <div class="col-10"></div>
+            <div class="col-80">
+                <ul id="studentList"></ul>
+            </div>
+            <div class="col-10"></div>
+            </div>
+            
+        </div>       
         <button class="close-btn" onclick="closeStudent()">Close</button>
-        <h3>Students for the selected time</h3>
-        <ul id="studentList"></ul>
     </div>
 </div>
-
-<script src="javascript/tutor-view-sched-studentlist.js"></script>
+<script src="javascript/tutor-view-sched-student list.js"></script>
 </body>
 </html>
