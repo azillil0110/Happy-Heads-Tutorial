78f542c262d366eb53352b6eb79a311449b70c17
Nopd101 <darylcruz101@gmail.com>
2024-12-05 00:58:18 +0800

    one more

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -73,12 +73,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $mail->isSMTP();
                     $mail->Host       = 'smtp.gmail.com';
                     $mail->SMTPAuth   = true;
-                    $mail->Username   = 'EMAIL';
-                    $mail->Password   = 'PASSWORD';
+                    $mail->Username   = 'johnlutherdelacruz01@gmail.com';
+                    $mail->Password   = 'ewhs vkjs uqcj hmjb';
                     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                     $mail->Port       = 587;
 
-                    $mail->setFrom('EMAIL', 'Happy Heads Tutorial Center');
+                    $mail->setFrom('johnlutherdelacruz01@gmail.com', 'Happy Heads Tutorial Center');
                     $mail->addAddress($_SESSION['email']);
                     $mail->isHTML(true);
                     $mail->Subject = 'Your Two-Factor Authentication Code';
